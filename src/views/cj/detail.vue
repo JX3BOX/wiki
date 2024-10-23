3ccf30461f5c672e57598ba68900f3be611553a8
Kaviilee <jiaweilee95@126.com>
2024-10-23 15:34:53 +0800

    perf: miniprogram适配

--- src/views/cj/detail.vue
+++ src/views/cj/detail.vue
@@ -231,11 +231,11 @@ export default {
 
         this.loadConfig();
 
-        const ua = navigator.userAgent;
-        // 当ua包含 Android 或 iPhone 或 Mobile 时
-        if (ua.indexOf("Android") > -1 || ua.indexOf("iPhone") > -1) {
-            location.href = `/wujie/cj/view/${this.id}`;
-        }
+        // const ua = navigator.userAgent;
+        // // 当ua包含 Android 或 iPhone 或 Mobile 时
+        // if (ua.indexOf("Android") > -1 || ua.indexOf("iPhone") > -1) {
+        //     location.href = `/wujie/cj/view/${this.id}`;
+        // }
     },
 };
 </script>
