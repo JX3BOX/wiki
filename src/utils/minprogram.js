e35d315fa97de77c68561b275fa002398cd5e303
x3zvawq <x3zvawq@gmail.com>
2025-12-09 15:21:48 +0800

    feat(miniprogram): web跳转问题

--- src/utils/minprogram.js
+++ src/utils/minprogram.js
@@ -1,6 +1,10 @@
 import wx from "weixin-js-sdk";
 import { authorLink, isMiniProgram } from "@jx3box/jx3box-common/js/utils";
 
+export function isInMiniprogramWebview() {
+    return window && window.__wxjs_environment === "miniprogram";
+}
+
 const serializeParams = (params) => {
     return Object.keys(params)
         .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -11,6 +15,11 @@ const serializeParams = (params) => {
  * 用于微信小程序的页面跳转,避免在同一个页面中跳转
  */
 export function wxNewPage(target) {
+    if (!isInMiniprogramWebview()) {
+        // 不在小程序中的时候回退操作使用location跳转
+        location.href = target;
+    }
+    
     const [path, queryString] = target.split("?");
 
     const query = queryString ? Object.fromEntries(new URLSearchParams(queryString)) : {};
